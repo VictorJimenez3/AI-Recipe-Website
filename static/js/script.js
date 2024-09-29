737503044086a9c2a3afeec4b871894a46a2723d
widkit <exylloath@gmail.com>
2024-09-29 10:36:34 -0400

    dosa

--- static/js/script.js
+++ static/js/script.js
@@ -24,7 +24,7 @@
                 }
             } catch (error) {
                 console.error('Error parsing JSON:', error);
-                alert("Unexpected error. Please try again later.");
+                alert("invalid input.");
             }
         })
         .catch(error => {
